40ee9c5758500b9ab19973cebc367ce33bee3e79
ktf <not.discostoo@gmail.com>
2011-09-07 18:02:10 -0700

    more tests

--- stories/tests.py
+++ stories/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
 
 
 """
-- if logged in, login link should redirect to profile page
 - can't edit others' story
 - can't edit others' profile
 - can't delete others' story
@@ -13,51 +13,71 @@ from django.test.client import Client
 
 - test the forms (signup, sign in, add/edit story, profile)
 - test the URLs?
+- test that URLs get correct views?
+
+doesn't exist yet:
+(- if logged in, login link should redirect to profile page)
 """
+
+
+class CommentsTest(TestCase):
+    def setUp(self):
+        pass
+
+
+class StoriesTest(TestCase):
+    def setUp(self):
+        self.c = Client()
+        user1 = User.objects.create_user('test1', 'test1@example.com', 'test1')
+        user2 = User.objects.create_user('test2', 'test2@example.com', 'test2')
+
+    def test_create_story(self):
+        pass
 
 
 class UrlTest(TestCase):
 
+    # these don't require authentication, or anything special
+    urls_basic = ('/',
+                  '/about/',
+                  '/search/',
+                  '/login/',
+                  '/logout/',
+                  '/register/',
+                  '/stories/',
+                  '/authors/',
+                 )
+
+    # these require the user to be logged in
+    urls_auth = ('/profile/',
+                 '/profile/edit/',
+                 '/stories/new/',
+                )
+
+    # TODO: these don't actually 404 yet...
+    should_404 = ('/stories/asdffsadfasdf/',
+                  '/stories/2342342342234/',
+                  '/authors/asdfasdfsda/',
+                  '/authors/23423423/',
+                  '/fsdafower/',
+                 )
+
     def setUp(self):
         self.c = Client()
+        user = User.objects.create_user('test', 'test@example.com', 'test')
 
     def test_basic_urls(self):
-        urls_200 = ('/',
-                    '/about/',
-                    '/search/',
-                   )
-        self.url_test_helper(urls_200, 200)
+        """basic URLs should return response code 200"""
+        self.url_test_helper(self.urls_basic, 200)
 
+    def test_auth_urls_anon(self):
+        """auth URLs should redirect if not logged in"""
+        self.url_test_helper(self.urls_auth, 302)
 
-    def test_auth_urls(self):
-        urls_200 = ('/login/', 
-                    '/logout/', 
-                    '/register/',
-                   )
-        urls_302 = ('/profile/',
-                    '/profile/edit/',
-                   )
-
-        self.url_test_helper(urls_200, 200)
-        self.url_test_helper(urls_302, 302)
-
-    def test_stories_urls(self):
-        urls_200 = ('/stories/',)
-        urls_302 = ('/stories/new/',)
-        urls_404 = ('/stories/asdfsdafsda/',
-                    '/stories/23423432432/')
-
-        self.url_test_helper(urls_200, 200)
-        self.url_test_helper(urls_302, 302)
-#        self.url_test_helper(urls_404, 404)
-
-    def test_authors_urls(self):
-        urls_200 = ('/authors/',)
-        urls_404 = ('/authors/sadfsadfsadfsad/',
-                    '/authors/23423423423/',
-                   )
-        self.url_test_helper(urls_200, 200)
-#        self.url_test_helper(urls_404, 404)
+    def test_auth_urls_logged_in(self):
+        """auth URLs should return response 200 if logged in"""
+        self.c.login(username='test', password='test')
+        self.url_test_helper(self.urls_auth, 200)
 
     def url_test_helper(self, urls, expected_response):
         """Loops through a list of URLs and makes sure each one returns the
